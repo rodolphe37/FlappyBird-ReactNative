a2ce2ef279cc4bd9a621a2f2a4136e3b32a226f9
rodolphe37 <rodolphe.a@gmail.com>
2019-11-11 17:04:28 +0100

    mise en place du decor, et des dernières fonctions necessaire

--- App.js
+++ App.js
@@ -1,151 +1,168 @@
-import React from 'react';
-import { StyleSheet, Text, View, Dimensions,StatusBar, Alert, TouchableOpacity } from 'react-native';
-import Constants from './Constants';
-import { GameEngine } from 'react-native-game-engine';
-import Matter from 'matter-js';
+import React, { Component } from 'react';
+import { Dimensions, StyleSheet, Text, View, StatusBar, Alert, TouchableOpacity, Image } from 'react-native';
+import Matter from "matter-js";
+import { GameEngine } from "react-native-game-engine";
 import Bird from './Bird';
-import Wall from './Wall';
-import Physics from './Physics';
+import Pipe from './Pipe';
+import Floor from './Floor';
+import Physics, { resetPipeCount } from './Physics';
+import Constants from './Constants';
+import Images from './assets/Images';
 
-export const randomBetween = (min, max) => {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-};
+export default class App extends Component {
+    constructor(props){
+        super(props);
 
-export const generatePipes = () => {
-  let topPipeHeight = randomBetween(100, (Constants.MAX_HEIGHT / 2) - 100);
-  let bottomPipeHeight = Constants.MAX_HEIGHT - topPipeHeight - Constants.GAP_SIZE;
+        this.state = {
+            running: true,
+            score: 0
+        };
 
-  let sizes = [topPipeHeight, bottomPipeHeight];
+        this.gameEngine = null;
 
-  if (Math.random() < 0.5) {
-    size = sizes.reverse();
-  }
-
-  return sizes;
-}
-
-
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.gameEngine = null;
-    this.entities = this.setupWorld();
-
-    this.state = {
-      running: true,
+        this.entities = this.setupWorld();
     }
-  }
 
-  setupWorld = () => {
-    let engine = Matter.Engine.create({enableSleeping: false});
-    let world = engine.world;
+    setupWorld = () => {
+        let engine = Matter.Engine.create({ enableSleeping: false });
+        let world = engine.world;
+        world.gravity.y = 0.0;
 
-    let bird = Matter.Bodies.rectangle(Constants.MAX_WIDTH / 4, Constants.MAX_HEIGHT / 2, 50, 50 );
-    let floor = Matter.Bodies.rectangle( Constants.MAX_WIDTH / 2, Constants.MAX_HEIGHT - 25, Constants.MAX_WIDTH, 50, {isStatic: true });
-    let ceiling = Matter.Bodies.rectangle( Constants.MAX_WIDTH / 2, 25, Constants.MAX_WIDTH, 50, {isStatic: true });
+        let bird = Matter.Bodies.rectangle( Constants.MAX_WIDTH / 2, Constants.MAX_HEIGHT / 2, Constants.BIRD_WIDTH, Constants.BIRD_HEIGHT);
+        //bird.restitution = 20;
 
-    let [pipe1Height, pipe2Height] = generatePipes();
-    let pipe1 = Matter.Bodies.rectangle( Constants.MAX_WIDTH - ( Constants.PIPE_WIDTH / 2), pipe1Height / 2 , Constants.PIPE_WIDTH, pipe1Height, {isStatic: true });
-    let pipe2 = Matter.Bodies.rectangle( Constants.MAX_WIDTH - ( Constants.PIPE_WIDTH / 2), Constants.MAX_HEIGHT - (pipe2Height / 2), Constants.PIPE_WIDTH, pipe2Height, {isStatic: true });
+        let floor1 = Matter.Bodies.rectangle(
+            Constants.MAX_WIDTH / 2,
+            Constants.MAX_HEIGHT - 25,
+            Constants.MAX_WIDTH + 4,
+            50, { isStatic: true }
+        );
+        let floor2 = Matter.Bodies.rectangle(
+            Constants.MAX_WIDTH + (Constants.MAX_WIDTH / 2),
+            Constants.MAX_HEIGHT - 25,
+            Constants.MAX_WIDTH + 4,
+            50, { isStatic: true }
+        );
 
-    let [pipe3Height, pipe4Height] = generatePipes();
-    let pipe3 = Matter.Bodies.rectangle( Constants.MAX_WIDTH * 2 - ( Constants.PIPE_WIDTH / 2), pipe3Height / 2 , Constants.PIPE_WIDTH, pipe3Height, {isStatic: true });
-    let pipe4 = Matter.Bodies.rectangle( Constants.MAX_WIDTH * 2 - ( Constants.PIPE_WIDTH / 2), Constants.MAX_HEIGHT - (pipe4Height / 2), Constants.PIPE_WIDTH, pipe4Height, {isStatic: true });
+        Matter.World.add(world, [bird, floor1]);
+        Matter.Events.on(engine, 'collisionStart', (event) => {
+            var pairs = event.pairs;
 
-    Matter.World.add(world, [bird, floor, ceiling, pipe1, pipe2, pipe3, pipe4]);
+            this.gameEngine.dispatch({ type: "game-over"});
 
-    Matter.Events.on(engine, "collisionStart", (event) =>{
-        let pairs = event.pairs;
+        });
 
-        this.gameEngine.dispatch({ type: "game-over"})
-    })
-
-    return {
-      physics: { engine: engine, world: world},
-      bird: { body: bird, size: [50, 50], color: 'red', renderer: Bird },
-      floor: { body: floor, size: [Constants.MAX_WIDTH, 50], color: 'green', renderer: Wall },
-      ceiling: { body: ceiling, size: [Constants.MAX_WIDTH, 50], color: 'green', renderer: Wall },
-      pipe1: { body: pipe1, size: [Constants.PIPE_WIDTH, pipe1Height], color: 'green', renderer: Wall },
-      pipe2: { body: pipe2, size: [Constants.PIPE_WIDTH, pipe2Height], color: 'green', renderer: Wall },
-      pipe3: { body: pipe3, size: [Constants.PIPE_WIDTH, pipe3Height], color: 'green', renderer: Wall },
-      pipe4: { body: pipe4, size: [Constants.PIPE_WIDTH, pipe4Height], color: 'green', renderer: Wall },
+        return {
+            physics: { engine: engine, world: world },
+            floor1: { body: floor1, renderer: Floor },
+            floor2: { body: floor2, renderer: Floor },
+            bird: { body: bird, pose: 1, renderer: Bird},
+        }
     }
-  }
 
-  onEvent = (e) => {
-      if ( e.type === "game-over") {
+    onEvent = (e) => {
+        if (e.type === "game-over"){
+            //Alert.alert("Game Over");
+            this.setState({
+                running: false
+            });
+        } else if (e.type === "score"){
+            this.setState({
+                score: this.state.score + 1
+            })
+        }
+    }
+
+    reset = () => {
+        this.gameEngine.swap(this.setupWorld());
+        resetPipeCount();
         this.setState({
-          running:false
-        })
-      }
-  }
+            running: true,
+            score: 0
+        });
+    }
 
-  reset = () => {
-    this.gameEngine.swap(this.setupWorld());
-    this.setState({
-      running: true
-    });
-  }
-
-  render() {
-    return (
-      <View style={styles.container}>
-      <GameEngine
-        ref={(ref) => { this.gameEngine = ref; }}
-        style={styles.gameContainer}
-        systems={[Physics]}
-        running={this.state.running}
-        onEvent={this.onEvent}
-        entities={this.entities}>
-        <StatusBar hidden={true} />
-      </GameEngine>
-      {!this.state.running && <TouchableOpacity onPress={this.state.reset} style={styles.fullScreenButton}>
-          <View style={styles.fullScreen}>
-            <Text style={styles.gameOverText}>Game Over</Text>
-          </View>
-      </TouchableOpacity>}
-      </View>
-    );
-  }
+    render() {
+        return (
+            <View style={styles.container}>
+                <Image style={styles.backgroundImage} resizeMode="stretch" source={Images.background} />
+                <GameEngine
+                    ref={(ref) => { this.gameEngine = ref; }}
+                    style={styles.gameContainer}
+                    systems={[Physics]}
+                    running={this.state.running}
+                    onEvent={this.onEvent}
+                    entities={this.entities}>
+                    <StatusBar hidden={true} />
+                </GameEngine>
+                <Text style={styles.score}>{this.state.score}</Text>
+                {!this.state.running && <TouchableOpacity style={styles.fullScreenButton} onPress={this.reset}>
+                    <View style={styles.fullScreen}>
+                        <Text style={styles.gameOverText}>Game Over</Text>
+                        <Text style={styles.gameOverSubText}>Try Again</Text>
+                    </View>
+                </TouchableOpacity>}
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-
-  gameContainer: {
-    position: 'absolute',
-    top: 0,
-    bottom: 0,
-    left: 0,
-    right: 0
-  },
-
-  fullScreenButton: {
-    position: 'absolute',
-    top: 0,
-    bottom: 0,
-    left: 0,
-    right: 0,
-    flex: 1
-  },
-
-  fullScreen: {
-    position: 'absolute',
-    top: 0,
-    bottom: 0,
-    left: 0,
-    right: 0,
-    backgroundColor: 'black',
-    opacity: 0.8,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  
-  gameOverText: {
-    color: "white",
-    fontSize: 48,
-  }
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+    },
+    backgroundImage: {
+        position: 'absolute',
+        top: 0,
+        bottom: 0,
+        left: 0,
+        right: 0,
+        width: Constants.MAX_WIDTH,
+        height: Constants.MAX_HEIGHT
+    },
+    gameContainer: {
+        position: 'absolute',
+        top: 0,
+        bottom: 0,
+        left: 0,
+        right: 0,
+    },
+    gameOverText: {
+        color: 'white',
+        fontSize: 48,
+    },
+    gameOverSubText: {
+        color: 'white',
+        fontSize: 24,
+    },
+    score: {
+        color: 'white',
+        fontSize: 72,
+        position: 'absolute',
+        top: 50,
+        left: Constants.MAX_WIDTH / 2 - 24,
+        textShadowColor:'#222222',
+        textShadowOffset:{width: 2, height: 2},
+        textShadowRadius:2,
+    },
+    fullScreen: {
+        position: 'absolute',
+        top: 0,
+        bottom: 0,
+        left: 0,
+        right: 0,
+        backgroundColor: 'black',
+        opacity: 0.8,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    fullScreenButton: {
+        position: 'absolute',
+        top: 0,
+        bottom: 0,
+        left: 0,
+        right: 0,
+        flex: 1
+    }
 });
