f0c37c448a9119fc144aefa05ae700f36042718b
rodolphe37 <rodolphe.a@gmail.com>
2019-11-11 12:33:11 +0100

    Début de mise en place des module Bird, Constants et Physics

--- App.js
+++ App.js
@@ -1,19 +1,49 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Dimensions,StatusBar, Alert, TouchableOpacity } from 'react-native';
+import Constants from './Constants';
+import { GameEngine } from 'react-native-game-engine';
+import Matter from 'matter-js';
+import Bird from './Bird';
+import Physics from './Physics';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.gameEngine = null;
+    this.entities = this.setupWorld();
+  }
+
+  setupWorld = () => {
+    let engine = Matter.Engine.create({enableSleeping: false});
+    let world = engine.world;
+
+    let bird = Matter.Bodies.rectangle(Constants.MAX_WIDTH / 4, Constants.MAX_HEIGHT / 2, 50, 50 );
+
+    Matter.World.add(world, [bird]);
+
+    return {
+      physics: { engine: engine, world: world},
+      bird: { body: bird, size: [50, 50], color: 'red', renderer: Bird }
+    }
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <GameEngine
+        ref={(ref) => { this.gameEngine = ref; }}
+        style={styles.gameContainer}
+        systems={[Physics]}
+        entities={this.entities} />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
